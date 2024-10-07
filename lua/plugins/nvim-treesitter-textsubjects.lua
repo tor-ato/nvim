b58e0314982fc2553fee476ef5774d30387707f3
Torato Kitahara <tkitahar@c3r4s3.42tokyo.jp>
2024-10-07 13:59:50 +0900

    for arch

--- lua/plugins/nvim-treesitter-textsubjects.lua
+++ lua/plugins/nvim-treesitter-textsubjects.lua
@@ -5,9 +5,10 @@ return {
     after = "nvim-treesitter",
     config = function()
         local textobjects = {
-            ["."] = "textsubjects-smart",
-            [";"] = "textsubjects-container-outer",
-            ["'"] = "textsubjects-container-inner",
+			['.'] = 'textsubjects-smart',
+            [';'] = 'textsubjects-container-outer',
+            ['i;'] = 'textsubjects-container-inner',
+            ['i;'] = { 'textsubjects-container-inner', desc = "Select inside containers (classes, functions, etc.)" },
         }
 
         require("nvim-treesitter.configs").setup({
