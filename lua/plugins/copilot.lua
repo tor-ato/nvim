41f8e29f997083a617868bc69e50a2297e33db97
tor-ato <toratokitahara@gmail.com>
2025-02-09 19:49:26 +0900

    [main] add copilot chat

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+    opts = {
+        suggestion = { enable = false },
+        panel = { enable = false },
+        filetypes = {
+            gitcommit = true,
+        },
+    },
+}
