dad097b841c3b360c5b861c43049ff973502e301
tor-ato <toratokitahara@gmail.com>
2025-03-02 14:52:30 +0900

    [main] mod:comment out copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,11 +1,11 @@
-return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-    opts = {
-        suggestion = { enable = false },
-        panel = { enable = false },
-        filetypes = {
-            gitcommit = true,
-        },
-    },
-}
+-- return {
+-- 	"zbirenbaum/copilot.lua",
+-- 	cmd = "Copilot",
+--     opts = {
+--         suggestion = { enable = false },
+--         panel = { enable = false },
+--         filetypes = {
+--             gitcommit = true,
+--         },
+--     },
+-- }
